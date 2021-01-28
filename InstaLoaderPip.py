bf856f64888121847ffd2a317c8bbabb4e8b7398
idan054 <idanbit80@gmai.com>
2021-01-29 00:00:22 +0200

    InstaLoaderPip.py as main.py
    V1.1 Looking for new posts and download when available
    מחפש האם הועלה פוסט חדש, ומוריד במידה וקיים
    
    V1.0 Download recent profile pics of any public page without login Instagram.
    יכולת להוריד כל תמונה מכל פרופיל ציבורי ללא התחברות לאינסטגרם

--- InstaLoaderPip.py
+++ InstaLoaderPip.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 from itertools import dropwhile, takewhile
 from datetime import date, timedelta
+from time import sleep
+
 import instaloader
 # Full Details: https://github.com/chris-greening/instascrape And https://github.com/chris-greening/instascrape/wiki/Scraped-data-points
 # from instascrape import *
@@ -8,13 +10,36 @@ import instaloader
 print("Start")
 L = instaloader.Instaloader()
 
-profile_page = instaloader.Profile.from_username(L.context, "nike").get_posts()
-print(profile_page.count)
+# username = input("Paste profile username: ")
+the_username = "spider.models"
 
-# למרות שיש צורך רק באיבור הראשון ברשימה
-# לא ניתן לוותר על הלולאה (ולהשתמש בערך כרשימה[0])
-for post in profile_page:
-    # Not work for Private Account! (כי אין צורך להתחבר)
-    L.download_post(post, target='Nike') # שם התיקייה אליה ייוצאו הקבצים
-    break
+profile_page = instaloader.Profile.from_username(L.context, the_username).get_posts()
+post_counter = profile_page.count
+static_post_counter = profile_page.count # הגדר כמות פוסטים ראשונית
+
+def new_post_checker():
+    global post_counter
+    global profile_page
+
+    while static_post_counter == post_counter:  # ראשונית = עדכנית
+        sleep(3)
+        profile_page = instaloader.Profile.from_username(L.context, the_username).get_posts()
+        post_counter = profile_page.count
+        print("No new post yet...")
+new_post_checker()
+
+print("\n New post Found!")
+print("---------------------- \n static_post_counter != post_counter")
+print(" static_post_counter is " + str(static_post_counter))
+print(" post_counter is " + str(post_counter) + "\n ----------------------")
+
+def download_lasted_post():
+    # למרות שיש צורך רק באיבור הראשון ברשימה
+    # לא ניתן לוותר על הלולאה (ולהשתמש בערך כרשימה[0])
+    for post in profile_page:
+        # Not work for Private Account! (כי אין צורך להתחבר)
+        # Takes time for multiple images/Videos
+        L.download_post(post, target=the_username)  # שם התיקייה אליה ייוצאו הקבצים
+        break
+download_lasted_post()
 
