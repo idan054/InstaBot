890154f5b4a9de2978b8297655722344e083c853
idan054 <idanbit80@gmai.com>
2021-02-05 15:34:53 +0200

    V1.6 setup_post() makes file with needed post details - main.py as main.py Directories of files in use: - myScripts - ReadyPosts - ThePost
    (V1.7 Telegram Bots on Gadgets directory)
    runs on linux ubuntu3 by Microsoft Azure
    
    V1.6.2 Nothing new. 0% Change, just log update:
    Setup Microsoft Azure VM to be able to run the code 24/7 (Always check if theres a new post) VM Based linux, Start using PuTTY & also connected to FileZila
    שום שינוי בקוד, רק עדכון ביומן. לאחר חיפושים אחר אופציות שונות, נבחר הענן Azure של מיקרוסופט שמציע יכולות מעולת וככל הנראה חינם ב12 החודשים הראשונים. מבוסס לינוקס ומתחבר ע"י PuTTY ובנוסף מחובר כבר גם ל FileZila
    
    V1.6.1 (Beta) (improvements)
    
    V1.6 when post found its scrape needed details and insert them to file (post_1.py) in ReadyPosts directory. based A4_Setup_post.py, setup_post(). Includes: Date, Time, Hashtags (Static, Beta), username & pic_link
    כשפוסט חדש מועלה, מוריד נתונים על הפוסט הנבחר (ברירת מחדל: ה3 החדש ביותר) ומייצא את הנתונים הדרושים לקובץ חדש בתיקייה ReadyPosts
    * Updated myToDo.txt עודכן
    
    V1.5.1 main.py Cleaner, (fit parts moved to B1_Post_Uploader.py) myToDo.txt plan the next steps...
    
    V1.5 Choose which post (relate to lasted) will be downloaded & autoUpload post include credit in desc to spider_modelsx, and delete tracks "ThePost" folder for next time. clean ver.
    גרסא נקייה המאפשרת לבחור איזה פוסט יורד (לדוגמא השלישי ביחס לחדש ביותר) במטרה למנוע חיכוכים עם יוצרי העמוד שיכול ליצור העלאת הפוסט החדש ביותר + העלאה אוטומטית כולל מתן קרדיט ומחיקת עקבות (תיקיית ThePost). גרסא נקייה.
    V1.4 Cleaner ver based main.py to run functions
    גרסא נקייה מבוססת main.py  להפעלת פונקציות והפרדת פונק' לקבצים נפרדים
    
    V1.4 (Beta) basic upload posts based 4LineInstaBot.py (Gadgets Directory) & Beta of cleaner version based separate Def files
    אופציה בסיסית להעלאת פוסטים לאינסטה! (עוד לא נוסה היטב) ותחילת מעבר ל main ראשי וקבצים עבור def ים  לגרסא נקייה יותק
    
    V1.3.1 (Fix error) Must Login or load session
    מסיבה לא ברורה כעת חייב ליצור סשן
    ע"מ ליצור קובץ סשן יש להשתמש ב
    "instaloader -l idan_biton16"
    
    V1.3 Insert multiple pages using input & Clean version based Def and global variables
    המשתמש מכניס את שמות עמודי האינטסה + גרסא נקייה על ידי Def ים וערכים גלובליים
    
    V1.2.3 (Bugs fix)
    
    V1.2.2 (Fix) Looking for new post in multiple pages and download post when there is
    כשמועלה פוסט חדש, מוריד אותו ועוצר את הלולאות
    
    V1.2.1 Able to looking for multiple pages based on for loop and dictionary list
    מחפש במס' עמודים מוגדרים מראש בהתבסס רשימה מילונית ולולאת לופ
    
    V1.1 Looking for new posts and download when available
    מחפש האם הועלה פוסט חדש, ומוריד במידה וקיים
    
    V1.0 Download recent profile pics of any public page without login Instagram.
    יכולת להוריד כל תמונה מכל פרופיל ציבורי ללא התחברות לאינסטגרם
    * הורדה מהחדש ביותר ואילך (ולא על ידי id של התמונה)

--- Gadgets/Telegram/MyWorkingBot.py
+++ Gadgets/Telegram/MyWorkingBot.py
@@ -1,19 +1,23 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from datetime import datetime
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+from Gadgets.console_colors import bcolors
 
 updater = Updater(token='1618156264:AAG4JRzGQD3O-gEjD3yFzS5OssJQiJML5j4', use_context=True) #Replace TOKEN with your token string
 telegarm_bot = updater.dispatcher
 
-print("Start")
+print(f"{bcolors.Blue} {bcolors.BOLD}Start {bcolors.Normal}")
 
 # secret hello command on bot (secret because not in @BotFather command list)
 def hello(update, context):
+    print("run def hello")
     context.bot.send_message(chat_id=update.effective_chat.id, text='Hello, World')
 hello_handler = CommandHandler('hello', hello) # CommandHandler
 telegarm_bot.add_handler(hello_handler)
 
 # date command on bot
 def date(update, context):
+    print("run def date")
     today = datetime.today().date()
     today_name = datetime.now().strftime("%A")
     def date_translator(ttoday_name):
@@ -32,6 +36,7 @@ telegarm_bot.add_handler(date_handler)
 
 # time command on bot
 def time(update, context):
+    print("run def time")
     my_time = datetime.now().strftime("%H:%M:%S") # 16:31:59
     context.bot.send_message(chat_id=update.effective_chat.id, text=f"{my_time}")
 time_handler = CommandHandler('time', time) # CommandHandler
@@ -39,12 +44,14 @@ telegarm_bot.add_handler(time_handler)
 
 # Any unknown command "MessageHandler(Filters.command, unknown)"
 def unknown(update, context):
+    print("run def unknown")
     context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
 unknown_handler = MessageHandler(Filters.command, unknown)
 telegarm_bot.add_handler(unknown_handler)
 
 # Any Text (unless Fortnite)
 def reply(update, context):
+    print("run def reply")
     user_input = update.message.text
     if "ortnit" in user_input or "פורטנ" in user_input:
         update.message.reply_text(f"{user_input}? \nרציני אחי? \nאתה מגעיל אותי.") #AKA print
